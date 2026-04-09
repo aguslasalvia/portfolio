561b33caa358d3e0fffe42263aca573aecd0a76a
aguslasalvia <agus.blumenfeld13@gmail.com>
2026-04-09 09:29:20 -0300

    add: contact form

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -92,6 +92,6 @@ export const portfolio = {
 
   contact: {
     text: "I'm always open to discussing new projects, creative ideas or opportunities to be part of your visions.",
-    email: 'agustin@email.com'
+    email: 'contact@aguslasalvia.online'
   }
 };
